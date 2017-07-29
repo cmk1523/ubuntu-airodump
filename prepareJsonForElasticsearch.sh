c918a330e7bf03ee29b1dac6d2d1c6dea831a735
cmk1523@gmail.com <cmk1523@gmail.com>
2017-07-29 13:11:31 -0400

    na
    na

--- prepareJsonForElasticsearch.sh
+++ prepareJsonForElasticsearch.sh
@@ -4,3 +4,6 @@
 
 file="$1"
 tail -n +2 "$file" | head -n -1 | sed -u 's/,$//' | sed -u -e 's/^/{ "index" : { "_index": "airodump", "_type": "v1" } }\n/' | cat
+
+# bulk ingest in to Elasticsearch
+curl -s -XPOST http://bos-01:9200/_bulk --data-binary "@output-01.elasticsearch.json"
